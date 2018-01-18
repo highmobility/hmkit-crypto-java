4d424df9bf7f83bc1536355797d221553cd3e16a
Tõnis Tiganik <ttiganik@gmail.com>
2018-01-18 18:03:40 +0700

    update documentation

--- src/main/java/com/highmobility/crypto/Crypto.java
+++ src/main/java/com/highmobility/crypto/Crypto.java
@@ -5,46 +5,90 @@ import com.highmobility.utils.Base64;
 
 import java.util.Random;
 
-/**
- * Created by ttiganik on 26/05/16.
- */
 public class Crypto {
 
     private static final HMBTCore core = new HMBTCore();
 
-    public static KeyPair createKeypair() {
+    /**
+     * Create a keypair.
+     *
+     * @return The KeyPair.
+     */
+    public static HMKeyPair createKeypair() {
         byte[] privateKey = new byte[32];
         byte[] publicKey = new byte[64];
 
         core.HMBTCoreCryptoCreateKeys(privateKey, publicKey);
-        return new KeyPair(privateKey, publicKey);
+        return new HMKeyPair(privateKey, publicKey);
     }
 
+    /**
+     * Create a random serial number.
+     *
+     * @return the 9 byte serial number.
+     */
     public static byte[] createSerialNumber() {
         byte[] serialBytes = new byte[9];
         new Random().nextBytes(serialBytes);
         return serialBytes;
     }
 
+    /**
+     * Add a signature for an access certificate.
+     *
+     * @param unsignedCert The access certificate
+     * @param privateKeyBase64 The private key that will be used for signing the certificate.
+     */
     public static void sign(AccessCertificate unsignedCert, String privateKeyBase64) {
         sign(unsignedCert, Base64.decode(privateKeyBase64));
     }
 
+    /**
+     * Add a signature for an access certificate.
+     *
+     * @param unsignedCert The access certificate
+     * @param privateKeyBytes The private key that will be used for signing the certificate.
+     */
     public static void sign(AccessCertificate unsignedCert, byte[] privateKeyBytes) {
         byte[] signature = sign(unsignedCert.getBytes(), privateKeyBytes);
         unsignedCert.setSignature(signature);
     }
 
+    /**
+     * Sign data.
+     *
+     * @param bytes The data that will be signed.
+     * @param keyPair The keypair that will be used for signing.
+     *
+     * @return The signature.
+     */
     public static byte[] sign(byte[] bytes, HMKeyPair keyPair) {
         return sign(bytes, keyPair.getPrivateKey());
     }
 
+    /**
+     * Sign data.
+     *
+     * @param bytes The data that will be signed.
+     * @param privateKey The private key that will be used for signing.
+     *
+     * @return The signature.
+     */
     public static byte[] sign(byte[] bytes, byte[] privateKey) {
         byte[] signature = new byte[64];
         core.HMBTCoreCryptoAddSignature(bytes, bytes.length, privateKey, signature);
         return signature;
     }
 
+    /**
+     * Verify a signature.
+     *
+     * @param data The data that was signed.
+     * @param signature The signature.
+     * @param publicKey The public key that is used for verifying.
+     *
+     * @return True if verified.
+     */
     public static boolean verify(byte[] data, byte[] signature, byte[] publicKey) {
         int result = core.HMBTCoreCryptoValidateSignature(data, data.length, publicKey, signature);
         return result == 0;
--- src/main/java/com/highmobility/crypto/HMKeyPair.java
+++ src/main/java/com/highmobility/crypto/HMKeyPair.java
@@ -3,28 +3,53 @@ package com.highmobility.crypto;
 
 import com.highmobility.utils.Base64;
 
+/**
+ * ECC private/public keypair that uses elliptic curve P-256.
+ */
 public class HMKeyPair {
     private static final long serialVersionUID = 6637283024188232326L;
     private byte[] privateKey;
     private byte[] publicKey;
 
-    public HMKeyPair(byte[] privateKey, byte[] publicKey) {
+    /**
+     * Create a ECC Keypair object with private and public key.
+     *
+     * @param privateKey The 32 bytes of the private key.
+     * @param publicKey The 64 bytes of the public key.
+     * @throws IllegalArgumentException When the keys are invalid.
+     */
         this.privateKey = privateKey;
         this.publicKey = publicKey;
     }
 
+    /**
+     *
+     * @return The public key.
+     */
     public byte[] getPublicKey() {
         return publicKey;
     }
 
+    /**
+     *
+     * @return The public key in Base64.
+     */
     public String getPublicKeyBase64() {
         return Base64.encode(publicKey);
     }
 
+    /**
+     *
+     * @return The private key.
+     */
     public byte[] getPrivateKey() {
         return privateKey;
     }
 
+    /**
+     *
+     * @return The private key in Base64.
+     */
     public String getPrivateKeyBase64() {
         return Base64.encode(privateKey);
     }
