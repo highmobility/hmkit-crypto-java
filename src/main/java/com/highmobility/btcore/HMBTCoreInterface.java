bc9423d40cc7d83842313a2c8cc4a3aa3df68cc3
Tõnis Tiganik <ttiganik@gmail.com>
2018-01-18 18:50:24 +0700

    add license headers

--- src/main/java/com/highmobility/btcore/HMBTCoreInterface.java
+++ src/main/java/com/highmobility/btcore/HMBTCoreInterface.java
@@ -1,3 +1,23 @@
+/*
+ * HMKit Crypto - Crypto for Java
+ * Copyright (C) 2018 High-Mobility <licensing@high-mobility.com>
+ *
+ * This file is part of HMKit Crypto.
+ *
+ * HMKit Crypto is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * HMKit Crypto is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with HMKit Crypto.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.highmobility.btcore;
 
 public interface HMBTCoreInterface {
